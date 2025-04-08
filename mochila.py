6f3b2c407d5f688e96126365e24817e9232f41ee
Valery <delgadodelacruzvaleryandrea@gmail.com>
2025-04-07 21:29:07 -0500

    Se realizo la etapa de inicializacion

---
+++ mochila.py
@@ -0,0 +1,28 @@
+import random
+from itertools import product
+
+objetos = [
+    {"nombre": "Cuaderno", "peso": 1.2, "valor": 15},
+    {"nombre": "Lápiz", "peso": 0.1, "valor": 5},
+    {"nombre": "Plumones", "peso": 2.0, "valor": 25},
+    {"nombre": "Regla", "peso": 0.5, "valor": 8},
+    {"nombre": "Escuadra", "peso": 1.8, "valor": 20},
+    {"nombre": "Calculadora", "peso": 0.7, "valor": 35},
+    {"nombre": "Compás", "peso": 0.6, "valor": 12},
+    {"nombre": "Libro de texto", "peso": 3.5, "valor": 50},
+    {"nombre": "Tablet", "peso": 1.3, "valor": 55},
+    {"nombre": "USB", "peso": 0.2, "valor": 10},
+    {"nombre": "Agenda", "peso": 0.8, "valor": 18},
+    {"nombre": "Colores", "peso": 1.1, "valor": 22}
+]
+
+CAPACIDAD_MAXIMA = 6
+TAMANO_POBLACION = 30
+TASA_MUTACION = 0.05
+GENERACIONES_LIMITE = 1000
+
+def generar_individuo():
+    return [random.randint(0, 1) for _ in range(len(objetos))]
+
+def generar_poblacion():
+    return [generar_individuo() for _ in range(TAMANO_POBLACION)]
